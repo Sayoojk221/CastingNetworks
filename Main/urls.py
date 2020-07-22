9d2822f0d634e2caadc959e3fcfb980e945879a0
sayooj k <sayoojk221@gmail.com>
2020-07-22 11:11:27 +0530

    login,register page created for user & company user homepage created & profile informations added

--- Main/urls.py
+++ Main/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 from Main import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('userhome/',views.user_home),
+    path('userlogin/',views.user_login),
+    path('userregister/',views.user_register),
+    path('companylogin/',views.company_login),
+    path('companyregister/',views.company_register),
+    path('userprofile/',views.user_profile),
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,5 +1,44 @@
 from django.shortcuts import render
-
+from Main.models import *
 
 def home(request):
-    return render(request,'base/index.html')
+    return render(request,'base/home.html')
+
+def user_home(request):
+    return render(request,'user/homepage.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+        user_details = UserRegister.objects.all().filter(name=username,password=password)
+        if user_details:
+            return render(request,'user/homepage.html')
+        else:
+            message = 'Invaild username and password!'
+            return render(request,'user/login.html',{'message':message})
+    else:
+        return render(request,'user/login.html')
+
+def user_register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        phonenumber = request.POST['phoneno']
+        password = request.POST['pass']
+        user_details = UserRegister(name=username,email=email,password=password,phone_number=phonenumber)
+        user_details.save()
+        return render(request,'user/register.html')
+    else:
+        return render(request,'user/register.html')
+
+def user_profile(request):
+    return render(request,'user/profile.html')
+
+def company_login(request):
+    return render(request,'company/login.html')
+
+def company_register(request):
+    return render(request,'company/register.html')
+
+
