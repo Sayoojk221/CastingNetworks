b8ea3a4c03d332d5c27684a089b24ae8b30a2c24
sayooj k <sayoojk221@gmail.com>
2020-07-28 11:46:48 +0530

    website homepage template modified added new template on user & company login admin page created and added some item like user approvel page , company approvel page and cast approvel page admin - added graphical view details about total approved user,company and castcalls user - added filter method in  userhomepage view function. when  user applied castcall that wiil be automatically disable from userhomepage company - created approvel page for user request

--- Main/views.py
+++ Main/views.py
@@ -1,44 +1,95 @@
 from django.shortcuts import render,redirect
 from Main.models import *
+from django.http import JsonResponse
+
+def admin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+        admin_details = Admin.objects.all().filter(username=username,password=password)
+        for i in admin_details:
+            request.session['admin-id']=i.id
+        if admin_details:
+            return redirect('/admindashboard/')
+
+        else:
+            return render(request,'admin/login.html')
+    else:
+        return render(request,'admin/login.html')
+
+def admin_dashboard(request):
+    if request.session.has_key('admin-id'):
+        total_user = UserRegister.objects.all().filter(status='approve').count()
+        total_company = FilmCompany.objects.all().filter(status='approve').count()
+        total_castcall = CastRegister.objects.all().filter(status='approve').count()
+        context = {'user':total_user,'company':total_company,'cast':total_castcall}
+        return render(request,'admin/dashboard.html',context)
+    else:
+        return redirect('/admin/')
+
+def admin_logout(request):
+    if request.session.has_key('admin-id'):
+        del request.session['admin-id']
+        return redirect('/admin/')
+    else:
+        return redirect('/admin/')
+
 
 def home(request):
-    return render(request,'base/home.html')
+    castcall_details = CastRegister.objects.all()
+    context = {'castcall':castcall_details}
+    return render(request,'base/home.html',context)
 
 def user_home(request):
-    return render(request,'user/homepage.html')
+    user = request.session['user-id']
+    booked_castcall_details = CastBooking.objects.all().filter(user_id=user).values('cast_id')
+    total_booked_castcall_id = []
+    for item in booked_castcall_details:
+        for i in item:
+            total_booked_castcall_id.append(item[i])
+
+    castcall_details = CastRegister.objects.all()
+    context = {'castcall':castcall_details,'total_id':total_booked_castcall_id}
+    return render(request,'user/homepage.html',context)
 
 def company_home(request):
-
-    return render(request,'company/companyhome.html')
+    companyid = request.session['company-id']
+    details = FilmCompany.objects.filter(id=companyid)
+    context = {'details':details}
+    return render(request,'company/companyhome.html',context)
 
 def user_login(request):
-
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['pass']
-        user_details = UserRegister.objects.all().filter(username=username,password=password)
-        for i in user_details:
-            request.session['user-id']=i.id
-        if user_details:
-            return render(request,'user/homepage.html')
-        else:
-            message = 'Invaild username and password!'
-            return render(request,'user/login.html',{'message':message})
+    if request.session.has_key('user-id'):
+        return redirect('/userhome/')
     else:
-        return render(request,'user/login.html')
+        if request.method == 'POST':
+            email = request.POST['email']
+            password = request.POST['pass']
+            user_details = UserRegister.objects.all().filter(email=email,password=password)
+            for i in user_details:
+                request.session['user-id']=i.id
+            if user_details:
+                return redirect('/userhome/')
+            else:
+                message = 'Invaild username and password!'
+                return render(request,'user/login.html',{'message':message})
+        else:
+            return render(request,'user/login.html')
 
 def user_register(request):
-    if request.method == 'POST':
-        fullname = request.POST['fullname']
-        username = request.POST['username']
-        email = request.POST['email']
-        phonenumber = request.POST['phoneno']
-        password = request.POST['pass']
-        user_details = UserRegister(profile_image='user/default.jpg',fullname=fullname,username=username,email=email,password=password,phone_number=phonenumber,status='pending')
-        user_details.save()
-        return render(request,'user/login.html')
+    if request.session.has_key('user-id'):
+       return redirect('/userhome/')
     else:
-        return render(request,'user/register.html')
+        if request.method == 'POST':
+            fullname = request.POST['name']
+            username = request.POST['username']
+            email = request.POST['email']
+            password = request.POST['pass']
+            user_details = UserRegister(profile_image='user/default.jpg',fullname=fullname,username=username,email=email,password=password,status='pending')
+            user_details.save()
+            return render(request,'user/login.html')
+        else:
+            return render(request,'user/register.html')
 
 def user_profile(request):
     if request.session.has_key('user-id'):
@@ -59,12 +110,47 @@ def user_profile(request):
     else:
         return render(request,'user/login.html')
 
+def company_profile(request):
+    if request.session.has_key('company-id'):
+        company = request.session['company-id']
+        company_details = FilmCompany.objects.filter(id=company)
+        context = {'details':company_details}
+        if request.method == 'POST':
+            companyname = request.POST['name']
+            email = request.POST['email']
+            address = request.POST['address']
+            phone = request.POST['phone']
+            FilmCompany.objects.all().filter(id=company).update(company_name=companyname,email=email,address=address,phone_number=phone)
+            return render(request,'company/profile.html',context)
+        else:
+            return render(request,'company/profile.html',context)
+    else:
+        return render(request,'company/login.html')
+
+
+def companyimage_upload(request):
+    if request.session.has_key('company-id'):
+        company = request.session['company-id']
+        company_logo = FilmCompany.objects.filter(id=company)
+        context = {'details':company_logo}
+        if request.method == 'POST':
+            company_details = FilmCompany.objects.get(id=company)
+            logo = request.FILES['image']
+            company_details.company_log = logo
+            company_details.save()
+            return render(request,'company/profile.html',context)
+        else:
+            return render(request,'company/profile.html',context)
+    else:
+        return render(request,'company/login.html')
+
 def userimage_upload(request):
     if request.session.has_key('user-id'):
         userid = request.session['user-id']
-        user_details = UserRegister.objects.filter(id=userid)
-        context = {'edit':user_details}
+        user = UserRegister.objects.filter(id=userid)
+        context = {'edit':user}
         if request.method == 'POST':
+            user_details = UserRegister.objects.get(id=userid)
             image = request.FILES['image']
             user_details.profile_image = image
             user_details.save()
@@ -74,6 +160,12 @@ def userimage_upload(request):
     else:
         return render(request,'user/login.html')
 
+def company_logout(request):
+    if request.session.has_key('company-id'):
+        del request.session['company-id']
+        return redirect('home')
+    else:
+        return render(request,'company/login.html')
 
 def user_logout(request):
     if request.session.has_key('user-id'):
@@ -83,32 +175,41 @@ def user_logout(request):
         return render(request,'user/login.html')
 
 def company_login(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['pass']
-        company_details = FilmCompany.objects.all().filter(email=email,password=password)
-        for i in company_details:
-            request.session['company-id']=i.id
-        if company_details:
-            return render(request,'company/companyhome.html')
-        else:
-            message = 'Invaild email and password!'
-            return render(request,'company/login.html',{'message':message})
+    if request.session.has_key('company-id'):
+        return redirect('/companyhome/')
     else:
-        return render(request,'company/login.html')
+        if request.method == 'POST':
+            email = request.POST['email']
+            password = request.POST['pass']
+            company_details = FilmCompany.objects.all().filter(email=email,password=password)
+            for i in company_details:
+                request.session['company-id']=i.id
+                companyid = i.id
+            if company_details:
+                details = FilmCompany.objects.filter(id=companyid)
+                context = {'details':details}
+                return render(request,'company/companyhome.html',context)
+            else:
+                message = 'Invaild email and password!'
+                return render(request,'company/login.html',{'message':message})
+        else:
+            return render(request,'company/login.html')
 
 def company_register(request):
-    if request.method == 'POST':
-        logo = request.FILES.get('logo')
-        companyname = request.POST['companyname']
-        email = request.POST['email']
-        password = request.POST['pass']
-        phone = request.POST['phonenumber']
-        company_details = FilmCompany(company_log=logo,company_name=companyname,email=email,password=password,phone_number=phone,status='pending')
-        company_details.save()
-        return render(request,'company/login.html')
+    if request.session.has_key('company-id'):
+        return redirect('/companyhome/')
     else:
-        return render(request,'company/register.html')
+        if request.method == 'POST':
+            logo = request.FILES.get('logo')
+            companyname = request.POST['companyname']
+            email = request.POST['email']
+            password = request.POST['pass']
+            phone = request.POST['phonenumber']
+            company_details = FilmCompany(company_log=logo,company_name=companyname,email=email,password=password,phone_number=phone,status='pending')
+            company_details.save()
+            return render(request,'company/login.html')
+        else:
+            return render(request,'company/register.html')
 
 def castcall_register(request):
     if request.session.has_key('company-id'):
@@ -150,15 +251,15 @@ def film_tags(request):
 def film_promotion(request):
     if request.session.has_key('company-id'):
         company = request.session['company-id']
-        tags = Tags.objects.all()
-        context = {'tags':tags}
+        total_tags = Tags.objects.all()
+        context = {'tags':total_tags}
         if request.method == 'POST':
             company_id = FilmCompany.objects.get(id=company)
             pro_video = request.FILES['pro_video']
-            tag = request.POST.getlist('tags')
+            tags = request.POST.getlist('tags')
             promotion_details = FilmPromotion(company_id=company_id,promotion_video=pro_video)
             promotion_details.save()
-            for tag in tag:
+            for tag in tags:
                 promotion_details.category.add(tag)
             return render(request,'company/filmpromotion.html',context)
         else:
@@ -166,3 +267,61 @@ def film_promotion(request):
 
     else:
         return render(request,'comapny/login.html')
+
+def castcall_view(request):
+    if request.session.has_key('user-id'):
+        castcall_id = request.GET['id']
+        clickedcastcall_details= CastRegister.objects.filter(id=castcall_id)
+        context = {'details':clickedcastcall_details}
+        return render(request,'user/castcallviewpage.html',context)
+
+def castcall_apply(request):
+    if request.session.has_key('user-id'):
+        user = request.session['user-id']
+        clickedcastcall_id = request.GET['id']
+        castcall_id = CastRegister.objects.get(id=clickedcastcall_id)
+        user_id = UserRegister.objects.get(id=user)
+        castcall_booking = CastBooking(cast_id=castcall_id,user_id=user_id,approve_status='pending')
+        castcall_booking.save()
+        return redirect('/userhome/')
+
+
+def adminuser_view(request):
+    if request.session.has_key('admin-id'):
+        user_details = UserRegister.objects.all().filter(status='pending')
+        user_id = request.GET.get('id')
+        UserRegister.objects.all().filter(id=user_id).update(status='approve')
+        context = {'user_details':user_details}
+        return render(request,'admin/userapprove.html',context)
+    else:
+        return redirect('/admin/')
+
+def admincompany_view(request):
+    if request.session.has_key('admin-id'):
+        company_details = FilmCompany.objects.all().filter(status='pending')
+        company_id = request.GET.get('id')
+        FilmCompany.objects.all().filter(id=company_id).update(status='approve')
+        context = {'company_details':company_details}
+        return render(request,'admin/companyapprove.html',context)
+    else:
+        return redirect('/admin/')
+
+def admincast_view(request):
+    if request.session.has_key('admin-id'):
+        totalcastcall_details = CastRegister.objects.all().filter(status='pending')
+        clickedcastcall_id = request.GET.get('id')
+        CastRegister.objects.all().filter(id=clickedcastcall_id).update(status='approve')
+        context={'cast_details':totalcastcall_details}
+        return render(request,'admin/castapprove.html',context)
+    else:
+        return redirect('/admin/')
+
+def usercastcall_approve(request):
+    if request.session.has_key('company-id'):
+        applied_castcall_id = request.GET.get('id')
+        CastBooking.objects.all().filter(id=applied_castcall_id).update(approve_status='approved')
+        total_applied_castcalls = CastBooking.objects.all().filter(approve_status='pending')
+        context = {'total_castcall':total_applied_castcalls}
+        return render(request,'company/usercastcall.html',context)
+    else:
+        return redirect('/companylogin/')
